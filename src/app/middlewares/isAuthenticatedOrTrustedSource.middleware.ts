0ea2bcbcff3a421008940a0aaab72d9a0365ab14
Stoian Catalin <stoiancatalin@192-168-0-108.rdsnet.ro>
2023-01-12 15:09:04 +0200

    add endpoints for orchestrator

---
+++ src/app/middlewares/isAuthenticatedOrTrustedSource.middleware.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import { EncryptionService, UserTokenClaims } from '@services/Encryption.service';
+
+export async function isAuthenticated(request: Request, response: Response, next: () => void) {
+  const token = request.headers['authorization'];
+
+  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+  if (!token) {
+    return response.sendStatus(401);
+  }
+
+  try {
+    const encryptionService = new EncryptionService();
+    request.user = encryptionService.verify<UserTokenClaims>(token);
+    next();
+  } catch (e) {
+    console.log(e);
+    return response.sendStatus(401);
+  }
+}
