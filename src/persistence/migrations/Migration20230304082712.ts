0225bf9ff6ec671921002ab7ab6f7c804b17e31e
Lucian Barticel <mail@lucianbarticel.ro>
2023-03-04 10:28:47 +0200

    update migrations with new fields

--- src/persistence/migrations/Migration20230304082712.ts
+++ src/persistence/migrations/Migration20230304082712.ts
@@ -1,14 +1,15 @@
 import { Migration } from '@mikro-orm/migrations';
 
-export class Migration20230304062255 extends Migration {
+export class Migration20230304082712 extends Migration {
 
   async up(): Promise<void> {
     this.addSql('alter table "user" alter column "sources_of_interest" type text[] using ("sources_of_interest"::text[]);');
 
-    this.addSql('alter table "document" add column "total_parts" int not null default 1, add column "part" int not null default 1;');
+    this.addSql('alter table "document" add column "processing_status" text check ("processing_status" in (\'downloaded\', \'locked\', \'ocr_in_progress\', \'ocr_done\', \'ocr_failed\')) not null default \'downloaded\', add column "total_parts" int not null default 1, add column "part" int not null default 1;');
   }
 
   async down(): Promise<void> {
+    this.addSql('alter table "document" drop column "processing_status";');
     this.addSql('alter table "document" drop column "total_parts";');
     this.addSql('alter table "document" drop column "part";');
 
