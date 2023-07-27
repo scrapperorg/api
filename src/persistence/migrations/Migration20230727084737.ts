beea8b0b5a6ffc3d06b0e67e7208054690218697
Stoian Catalin <stoiancatalin@192-168-0-111.rdsnet.ro>
2023-07-27 11:52:23 +0300

    don't drop attachments

--- src/persistence/migrations/Migration20230727084737.ts
+++ src/persistence/migrations/Migration20230727084737.ts
@@ -1,17 +1,15 @@
 import { Migration } from '@mikro-orm/migrations';
 
 export class Migration20230727084737 extends Migration {
-
   async up(): Promise<void> {
-    this.addSql('alter table "project" drop column "attachments";');
-
-    this.addSql('alter table "user" alter column "sources_of_interest" type text[] using ("sources_of_interest"::text[]);');
+    this.addSql(
+      'alter table "user" alter column "sources_of_interest" type text[] using ("sources_of_interest"::text[]);',
+    );
   }
 
   async down(): Promise<void> {
-    this.addSql('alter table "project" add column "attachments" text[] not null default \'{}\';');
-
-    this.addSql('alter table "user" alter column "sources_of_interest" type text[] using ("sources_of_interest"::text[]);');
+    this.addSql(
+      'alter table "user" alter column "sources_of_interest" type text[] using ("sources_of_interest"::text[]);',
+    );
   }
-
 }
